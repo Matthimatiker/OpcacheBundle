e7150b196b75c11088b37888ce4ffaa913669145
Matthias Molitor <mmo@webfactory.de>
2015-08-08 19:03:38 +0200

    added comments

--- ByteCodeCache/ByteCodeCacheInterface.php
+++ ByteCodeCache/ByteCodeCacheInterface.php
@@ -2,19 +2,28 @@
 
 namespace Matthimatiker\OpcacheBundle\ByteCodeCache;
 
+/**
+ * Defines an access layer for byte code cache information.
+ */
 interface ByteCodeCacheInterface
 {
     /**
+     * Checks if the byte code cache is active.
+     *
      * @return boolean
      */
     public function isEnabled();
 
     /**
+     * Provides information about the memory usage.
+     *
      * @return MemoryUsage
      */
     public function memory();
 
     /**
+     * Provides caching statistics.
+     *
      * @return Statistics
      */
     public function statistics();
--- ByteCodeCache/Statistics.php
+++ ByteCodeCache/Statistics.php
@@ -7,5 +7,33 @@ namespace Matthimatiker\OpcacheBundle\ByteCodeCache;
  */
 class Statistics
 {
+    /**
+     * Returns the number of cache hits.
+     *
+     * @return integer
+     */
+    public function getHits()
+    {
 
+    }
+
+    /**
+     * Returns the number of cache misses.
+     *
+     * @return integer
+     */
+    public function getMisses()
+    {
+
+    }
+
+    /**
+     * Returns the cache hit rate in percent.
+     *
+     * @return double
+     */
+    public function getHitRateInPercent()
+    {
+
+    }
 }
