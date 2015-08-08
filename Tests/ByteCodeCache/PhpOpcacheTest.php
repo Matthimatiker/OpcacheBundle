2aca6e86438bef27915df15187dad5a1dc7f3524
Matthias Molitor <mmo@webfactory.de>
2015-08-08 21:08:39 +0200

    added test case stub

---
+++ Tests/ByteCodeCache/PhpOpcacheTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Matthimatiker\OpcacheBundle\Tests\ByteCodeCache;
+
+use Matthimatiker\OpcacheBundle\ByteCodeCache\PhpOpcache;
+
+class PhpOpcacheTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * System under test.
+     *
+     * @var PhpOpcache
+     */
+    protected $opcache = null;
+
+    /**
+     * Initializes the test environment.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+    }
+
+    /**
+     * Cleans up the test environment.
+     */
+    protected function tearDown()
+    {
+
+        parent::tearDown();
+    }
+
+    public function testImplementsInterface()
+    {
+
+    }
+
+    public function testProvidesCorrectMemoryUsage()
+    {
+
+    }
+
+    public function testProvidesCorrectMemorySize()
+    {
+
+    }
+
+    public function testProvidesCorrectMemoryUsageInPercent()
+    {
+
+    }
+
+    public function testProvidesCorrectNumberOfHits()
+    {
+
+    }
+
+    public function testProvidesCorrectNumberOfMisses()
+    {
+
+    }
+
+    public function testProvidesCorrectHitRate()
+    {
+
+    }
+}
