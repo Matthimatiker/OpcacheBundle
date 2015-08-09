0c8b2b0caa1c2a33e7ef8de261fab19b82111dbd
Matthias Molitor <mmo@webfactory.de>
2015-08-09 15:46:39 +0200

    registered data collector stub and added example template

--- DataCollector/ByteCodeCacheDataCollector.php
+++ DataCollector/ByteCodeCacheDataCollector.php
@@ -2,10 +2,34 @@
 
 namespace Matthimatiker\OpcacheBundle\DataCollector;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\DataCollector\DataCollector;
+
 /**
  * Collects data about the byte code cache usage.
  */
-class ByteCodeCacheDataCollector
+class ByteCodeCacheDataCollector extends DataCollector
 {
+    /**
+     * Collects data for the given Request and Response.
+     *
+     * @param Request $request A Request instance
+     * @param Response $response A Response instance
+     * @param \Exception $exception An Exception instance
+     */
+    public function collect(Request $request, Response $response, \Exception $exception = null)
+    {
+        // TODO: Implement collect() method.
+    }
 
+    /**
+     * Returns the name of the collector.
+     *
+     * @return string The collector name
+     */
+    public function getName()
+    {
+        return 'matthimatiker_opcache.byte_code_cache';
+    }
 }
