a22b382d42611848a055bbb29d34b3213aa3e4e0
Matthias Molitor <mmo@webfactory.de>
2015-08-09 17:39:57 +0200

    added stub for class that maps cache data to an array (for serialization during profiling)

---
+++ ByteCodeCache/ArrayMapper.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Matthimatiker\OpcacheBundle\ByteCodeCache;
+
+/**
+ * Maps cache data to a simple array structure and back.
+ */
+class ArrayMapper
+{
+    /**
+     * @param ByteCodeCacheInterface $cache
+     * @return array
+     */
+    public function toArray(ByteCodeCacheInterface $cache)
+    {
+
+    }
+
+    /**
+     * @param array $data
+     * @return ByteCodeCacheInterface
+     */
+    public function fromArray(array $data)
+    {
+
+    }
+}
