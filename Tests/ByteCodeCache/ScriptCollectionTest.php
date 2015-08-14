7fb31672670861e0b8ddb0c177c5c503c26f606c
Matthias Molitor <mmo@webfactory.de>
2015-08-14 15:29:27 +0200

    implemented tests

--- Tests/ByteCodeCache/ScriptCollectionTest.php
+++ Tests/ByteCodeCache/ScriptCollectionTest.php
@@ -2,7 +2,9 @@
 
 namespace Matthimatiker\OpcacheBundle\Tests\ByteCodeCache;
 
+use Matthimatiker\OpcacheBundle\ByteCodeCache\Script;
 use Matthimatiker\OpcacheBundle\ByteCodeCache\ScriptCollection;
+use Matthimatiker\OpcacheBundle\ByteCodeCache\ScriptSlots;
 
 class ScriptCollectionTest extends \PHPUnit_Framework_TestCase
 {
@@ -19,7 +21,7 @@ class ScriptCollectionTest extends \PHPUnit_Framework_TestCase
     protected function setUp()
     {
         parent::setUp();
-
+        $this->scripts = new ScriptCollection($this->createScripts(), 4);
     }
 
     /**
@@ -27,37 +29,58 @@ class ScriptCollectionTest extends \PHPUnit_Framework_TestCase
      */
     protected function tearDown()
     {
-
+        $this->scripts = null;
         parent::tearDown();
     }
 
     public function testIsTraversable()
     {
-
+        $this->assertInstanceOf(\Traversable::class, $this->scripts);
     }
 
     public function testProvidesAccessToScriptInformation()
     {
-
+        $this->assertInstanceOf(\Traversable::class, $this->scripts);
+        $this->assertContainsOnly(Script::class, $this->scripts);
     }
 
     public function testCountReturnsNumberOfScripts()
     {
-
+        $this->assertInstanceOf(\Countable::class, $this->scripts);
+        $this->assertCount(2, $this->scripts);
     }
 
-    public function testGetMaxNumberOfSlotsReturnsCorrectValue()
+    public function testGetSlotsReturnsEqualObjectOnEachCall()
     {
-
+        $this->assertEquals($this->scripts->getSlots(), $this->scripts->getSlots());
     }
 
-    public function testGetNumberOfFreeSlotsReturnsCorrectValue()
+    public function testMaxNumberOfSlotsIsCorrect()
     {
+        $slots = $this->scripts->getSlots();
 
+        $this->assertInstanceOf(ScriptSlots::class, $slots);
+        $this->assertEquals(4, $slots->max());
     }
 
-    public function testSlotUsageInPercentIsCalculatedCorrectly()
+    public function testNumberOfUsedSlotsIsCorrect()
     {
+        $slots = $this->scripts->getSlots();
 
+        $this->assertInstanceOf(ScriptSlots::class, $slots);
+        $this->assertEquals(2, $slots->used());
+    }
+
+    /**
+     * Creates some scripts for testing.
+     *
+     * @return Script[]
+     */
+    protected function createScripts()
+    {
+        return array(
+            new Script('/tmp/first.php', 0.1, 2, '2015-01-01 00:00:00'),
+            new Script('/tmp/second.php', 0.5, 42, '2015-06-01 00:00:00'),
+        );
     }
 }
