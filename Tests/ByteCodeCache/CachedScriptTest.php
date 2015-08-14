b8345729db2faf175d3899dc8287511005bcb511
Matthias Molitor <mmo@webfactory.de>
2015-08-14 13:46:25 +0200

    implemented tests

--- Tests/ByteCodeCache/CachedScriptTest.php
+++ Tests/ByteCodeCache/CachedScriptTest.php
@@ -2,15 +2,29 @@
 
 namespace Matthimatiker\OpcacheBundle\Tests\ByteCodeCache;
 
+use Matthimatiker\OpcacheBundle\ByteCodeCache\CachedScript;
+
 class CachedScriptTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * System under test.
+     *
+     * @var CachedScript
+     */
+    protected $script = null;
+
     /**
      * Initializes the test environment.
      */
     protected function setUp()
     {
         parent::setUp();
-
+        $this->script = new CachedScript(
+            '/tmp/my/script/file.php',
+            0.75,
+            42,
+            new \DateTime('2015-08-14 13:30:00')
+        );
     }
 
     /**
@@ -18,47 +32,82 @@ class CachedScriptTest extends \PHPUnit_Framework_TestCase
      */
     protected function tearDown()
     {
-
+        $this->script = null;
         parent::tearDown();
     }
 
     public function testGetFileReturnsFileObject()
     {
+        $file = $this->script->getFile();
 
+        $this->assertInstanceOf(\SplFileInfo::class, $file);
     }
 
     public function testFileObjectHasCorrectPath()
     {
+        $file = $this->script->getFile();
 
+        $this->assertInstanceOf(\SplFileInfo::class, $file);
+        $this->assertEquals('/tmp/my/script/file.php', $file->getPathname());
     }
 
     public function testGetMemoryConsumptionInMbReturnsCorrectValue()
     {
-
+        $this->assertEquals(
+            0.75,
+            $this->script->getMemoryConsumptionInMb(),
+            'Invalid memory consumption returned.',
+            0.001
+        );
     }
 
     public function testGetHitsReturnsCorrectValue()
     {
-
+        $this->assertEquals(42, $this->script->getHits());
     }
 
     public function testGetLastAccessReturnsDateTime()
     {
+        $lastAccess = $this->script->getLastAccess();
 
+        $this->assertInstanceOf(\DateTimeInterface::class, $lastAccess);
     }
 
     public function testGetLastAccessReturnsCorrectDate()
     {
+        $lastAccess = $this->script->getLastAccess();
 
+        $this->assertInstanceOf(\DateTimeInterface::class, $lastAccess);
+        $this->assertEquals('2015-08-14 13:30:00', $lastAccess->format('Y-m-d H:i:s'));
     }
 
     public function testGetLastAccessReturnsCorrectDateIfProvidedAsString()
     {
+        $this->script = new CachedScript(
+            '/any/path/file.php',
+            0.1,
+            0,
+            '2015-01-01 12:00:00'
+        );
+        $lastAccess = $this->script->getLastAccess();
 
+        $this->assertInstanceOf(\DateTimeInterface::class, $lastAccess);
+        $this->assertEquals('2015-01-01 12:00:00', $lastAccess->format('Y-m-d H:i:s'));
     }
 
     public function testGetFileReturnsCorrectObjectIfProvidedAsFileObject()
     {
+        $originalFile = new \SplFileInfo('/any/path/file.php');
+        $this->script = new CachedScript(
+            $originalFile,
+            0.1,
+            0,
+            '2015-01-01 12:00:00'
+        );
 
+        $file = $this->script->getFile();
+
+        $this->assertInstanceOf(\SplFileInfo::class, $file);
+        $this->assertEquals($originalFile->getPathname(), $file->getPathname());
     }
 }
