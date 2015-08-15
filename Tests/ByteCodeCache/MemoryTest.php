ac1692f5f6bd280591daadaa991271d8f2608fc4
Matthias Molitor <mmo@webfactory.de>
2015-08-15 10:28:43 +0200

    added tests

--- Tests/ByteCodeCache/MemoryTest.php
+++ Tests/ByteCodeCache/MemoryTest.php
@@ -19,7 +19,7 @@ class MemoryTest extends \PHPUnit_Framework_TestCase
     protected function setUp()
     {
         parent::setUp();
-        $this->memory = new Memory(150.0, 200.0);
+        $this->memory = new Memory(150.0, 225.0, 50.0);
     }
 
     /**
@@ -36,19 +36,49 @@ class MemoryTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(150.0, $this->memory->getUsedInMb());
     }
 
-    public function testGetSizeReturnsCorrectValue()
+    public function testGetSizeInMbReturnsCorrectValue()
     {
-        $this->assertEquals(200.0, $this->memory->getSizeInMb());
+        $this->assertEquals(225.0, $this->memory->getSizeInMb());
     }
 
-    public function testUsedMemoryInPercentIsCalculatedCorrectly()
+    public function testGetWastedInMbReturnsCorrectValue()
     {
-        $this->assertEquals(75.0, $this->memory->getUsedInPercent(), 'Percentage calculation invalid.', 0.001);
+        $this->assertEquals(50.0, $this->memory->getWastedInMb());
     }
 
     public function testFreeMemoryIsCalculatedCorrectly()
     {
-        $this->assertEquals(50.0, $this->memory->getFreeInMb(), 'Free memory calculation invalid.', 0.001);
+        $this->assertEquals(25.0, $this->memory->getFreeInMb(), 'Free memory calculation invalid.', 0.001);
+    }
+
+    public function testUsedMemoryInPercentIsCalculatedCorrectly()
+    {
+        $this->assertEquals(
+            (150.0 / 225.0) * 100.0,
+            $this->memory->getUsedInPercent(),
+            'Percentage calculation invalid.',
+            0.001
+        );
+    }
+
+    public function testWastedMemoryInPercentIsCalculatedCorrectly()
+    {
+        $this->assertEquals(
+            (50.0 / 225.0) * 100.0,
+            $this->memory->getUsedInPercent(),
+            'Percentage calculation invalid.',
+            0.001
+        );
+    }
+
+    public function testFreeMemoryInPercentIsCalculatedCorrectly()
+    {
+        $this->assertEquals(
+            (25.0 / 225.0) * 100.0,
+            $this->memory->getUsedInPercent(),
+            'Percentage calculation invalid.',
+            0.001
+        );
     }
 
     public function testIsFullReturnsFalseIfSomeMemoryIsFree()
@@ -63,16 +93,23 @@ class MemoryTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($this->memory->isFull());
     }
 
-    public function testIsFullReturnsFalseIfMoreMemoryThanAvailableIsUsed()
+    public function testIsFullReturnsTrueIfMoreMemoryThanAvailableIsUsed()
     {
         $this->memory = new Memory(2.0, 1.0);
 
         $this->assertTrue($this->memory->isFull());
     }
 
+    public function testIsFullReturnsTrueIfUsedAndWastedMemoryReachSize()
+    {
+        $this->memory = new Memory(150.0, 200.0, 50.0);
+
+        $this->assertTrue($this->memory->isFull());
+    }
+
     public function testMemoryUsageInPercentIsCalculatedCorrectlyIfSizeIsZero()
     {
-        $this->memory = new Memory(0, 0);
+        $this->memory = new Memory(0.0, 0.0);
 
         $this->assertEquals(0.0, $this->memory->getUsedInPercent(), 'Percentage calculation invalid.', 0.001);
     }
